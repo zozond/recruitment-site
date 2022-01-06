38d0e8374fe110c9a381ce1ec7a124da3bf308c3
DESKTOP-VCB2352\krpw1 <krpw11235788@gmail.com>
2022-01-06 23:55:43 +0900

    init

---
+++ src/main/java/com/recruitment/site/RecruitmentSiteApplication.java
@@ -0,0 +1,13 @@
+package com.recruitment.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentSiteApplication.class, args);
+	}
+
+}
